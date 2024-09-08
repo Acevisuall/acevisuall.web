e18fdb736c94bdbb5cedcdfcf697a46e8ea2e2a1
Warren Lee <warrenlee86@gmail.com>
2024-09-08 12:59:46 +0800

    Add media to config

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {singletonTools} from 'sanity-plugin-singleton-tools'
+import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemaTypes'
 import {SetAndPublishAction} from './actions/SetAndPublishAction'
 
@@ -12,7 +13,7 @@ export default defineConfig({
   projectId: 'yj5ogbk2',
   dataset: process.env.NODE_ENV === 'development' ? 'development' : 'production',
 
-  plugins: [structureTool(), visionTool(), singletonTools()],
+  plugins: [structureTool(), visionTool(), singletonTools(), media()],
 
   schema: {
     types: schemaTypes,
