9b4de75c0879a077637136677f8959cbe5ed7300
Warren Lee <warrenlee86@gmail.com>
2024-09-08 13:19:50 +0800

    Setup tailwind, add swiper

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,9 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: {enabled: true},
-  modules: ['@nuxtjs/sanity', '@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  modules: ['@nuxtjs/sanity', '@nuxtjs/tailwindcss', '@nuxt/eslint', 'nuxt-swiper'],
   sanity: {
     projectId: 'yj5ogbk2',
     dataset: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        container: {
+          center: true,
+          padding: '20px',
+        },
+      },
+    },
   },
 })
